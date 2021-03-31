4b0da48217c81047187e97a0cf25cb9245938d27
JordyCastillo33 <jordyjosuecastillo10@gmail.com>
2021-03-31 15:42:14 -0600

    Creacion de la interfaz grafica del login, con sus respectivas funcionalidades , se estableció una conexion con la base de datos y se creo una clase llamada Empleado.

---
+++ VitalCareRx/Empleado.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregar los namespaces de conexión con SQL Server
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace VitalCareRx
+{
+    class Empleado
+    {
+
+        //Variables miembro
+
+        private static string connectionString = ConfigurationManager.ConnectionStrings["VitalCareRx.Properties.Settings.VitalCareRxConnectionString"].ConnectionString;
+
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        public int IdEmpleado { get; set; }
+
+        public string PrimerNombre { get; set; }
+
+        public string SegundoNombre { get; set; }
+
+        public string PrimerApellido { get; set; }
+
+        public string SegundoApellido { get; set; }
+
+        public string Celular { get; set; }
+
+        public int IdSexo { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Contrasenia { get; set; }
+
+        public Empleado() { }
+
+        public Empleado(string primerNombre, string segundoNombre, string primerApellido, string segundoApellido, string celular, int idSexo, string nombreUsuario, string contrasenia )
+        {
+            PrimerNombre = primerNombre;
+            SegundoNombre = segundoNombre;
+            PrimerApellido = primerApellido;
+            SegundoApellido = segundoApellido;
+            Celular = celular;
+            IdSexo = idSexo;
+            NombreUsuario = nombreUsuario;
+            Contrasenia = contrasenia;
+
+
+        }
+
+        public Empleado BuscarEmpleado(string nombreUsuario)
+        {
+            Empleado empleado = new Empleado();
+            try
+            {
+                // Query de selección
+                string query = @"SELECT * FROM [Personas].[Empleado] WHERE nombreUsuario = @user";
+
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("@user", nombreUsuario);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del empleado si la consulta retorna valores
+                        empleado.IdEmpleado = Convert.ToInt32(rdr["idEmpleado"]);
+                        empleado.PrimerNombre = rdr["primerNombre"].ToString();
+                        empleado.SegundoNombre = rdr["segundoNombre"].ToString();
+                        empleado.PrimerApellido = rdr["primerApellido"].ToString();
+                        empleado.SegundoApellido = rdr["segundoApellido"].ToString();
+                        empleado.Celular = rdr["celular"].ToString();
+                        empleado.IdSexo = Convert.ToInt32(rdr["idSexo"]);
+                        empleado.NombreUsuario = rdr["nombreUsuario"].ToString();
+                        empleado.Contrasenia = rdr["contrasenia"].ToString();
+
+
+                    }
+                }
+
+                // Retornar el usuario con los valores
+                return empleado;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                
+                
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+    }
+}
---
+++ VitalCareRx/Login.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VitalCareRx
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private Empleado empleado = new Empleado();
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Implementar la búsqueda del usuario desde la clase Usuario
+                Empleado unEmpleado = empleado.BuscarEmpleado(txtUsuario.Text);
+
+                // Verificar si el usuario existe
+                if (unEmpleado.NombreUsuario == null)
+                    MessageBox.Show("El usuario o la contraseña no es correcta. Favor verificar.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                {
+                    // Verificar que la contraseña ingresada es igual a la contraseña
+                    // almacenada en la base de datos
+                    if (unEmpleado.Contrasenia == pwbPassword.Password)
+                    {
+                        // Mostrar el formulario de carga
+
+                        
+                        Loading loading = new Loading(txtUsuario);
+                        loading.Show();
+                        this.Close();
+
+                        
+
+              
+                    }
+
+                    else
+                        MessageBox.Show("El usuario o la contraseña no es correcta. Favor verificar.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ha ocurrido un error al momento de realizar la consulta... Favor intentelo de nuevo mas tarde", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                               
+            }
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        private void btnNuevaCuenta_Click(object sender, RoutedEventArgs e)
+        {
+            NuevoEmpleado nuevoEmpleado = new NuevoEmpleado();
+            nuevoEmpleado.show();
+            this.Close();
+        }
+    }
+}
