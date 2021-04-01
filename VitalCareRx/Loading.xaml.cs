1d181b100626b3f7d3adf0cfff761abd07af9d66
AngelPagoaga <angelpagoaga8@gmail.com>
2021-03-31 20:18:30 -0600

    Creación y codificación de ventana de carga hacia el Menu Principal.

---
+++ VitalCareRx/Loading.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace VitalCareRx
+{
+    /// <summary>
+    /// Lógica de interacción para Loading.xaml
+    /// </summary>
+    public partial class Loading : Window
+    {
+
+        
+        private Timer timer;
+        private int a = 0;
+        private string str;
+        public Loading(string empleado)
+        {
+            InitializeComponent();
+            timer = new Timer(100);
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Metodo para Visualizar el progreso de la ProgressBar.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, (Action)(() =>
+            {
+                if (BAR.Value < 30)
+                {
+                    a += 1;
+                    str = a + "%";
+                    lbLoad.Content = str;
+                    //lbPorciento.Visibility = Visibility;
+                    BAR.Value += 0.3;
+
+                }
+                else
+                {
+                    timer.Stop();
+                    this.Close();
+                }
+            }));
+        }
+
+
+    }
+}
