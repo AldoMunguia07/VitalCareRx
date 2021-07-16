1102584444e685df2bcd4dcb5a221a9ea9b96339
Aldo Munguía <aldojosuemh@gmail.com>
2021-07-16 08:14:25 -0600

    Creacion de formulario empleados

---
+++ VitalCareRx/Empleados.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VitalCareRx
+{
+    /// <summary>
+    /// Lógica de interacción para Empleados.xaml
+    /// </summary>
+    public partial class Empleados : Window
+    {
+        public Empleados()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Si se le da click derecho que no permita mover la ventana
+
+
+        }
+
+        //cuando se mantiene presionado click derecho
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        //cuando se suelta el click derecho
+        private void Window_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtPrimerNombre_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+
+        }
+
+        private void txtPrimerNombre_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtCelular_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+
+        }
+
+        private void txtCelular_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtSegundoNombre_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void txtSegundoNombre_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtPrimerApellido_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void txtPrimerApellido_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtSegundoApellido_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void txtSegundoApellido_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+
+        private void txtUsuario_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnModificarr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
