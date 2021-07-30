17c21b1c36b0c280c28f4b0254ff9a3940b9c2bb
angel <angel@ANGELPAGOAGA>
2021-07-30 14:53:01 -0600

    Correción de cadena de conexión en reportes.

--- VitalCareRx/Properties/Settings.Designer.cs
+++ VitalCareRx/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace VitalCareRx.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace VitalCareRx.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source = (local)\\sqlexpress; Initial Catalog = VitalCareRxDB\n; Integrated Se" +
-            "curity = true;\n")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DBServer;Initial Catalog=VitalCareRxDB;Persist Security Info=True;Use" +
+            "r ID=sqlAccessVitalCare;Password=care123\n")]
         public string VitalCareRxConnectionString {
             get {
                 return ((string)(this["VitalCareRxConnectionString"]));
